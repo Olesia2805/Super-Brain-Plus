21c43ff44d959061248665f0e325e47385e024d0
Anna <101461125+AnnaNADVOR@users.noreply.github.com>
2024-08-28 00:25:46 +0300

    Addheader (#8)
    * add footer fix mobile header
    
    * add mobile menu

--- src/js/gallery-slider.js
+++ src/js/gallery-slider.js
@@ -1,8 +1,8 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.css';
-import '../css/layout/gallery-slider.css';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.css';
+// import '../css/layout/gallery-slider.css';
 
-new SimpleLightbox('.gallery-list a', {
-  showCounter: false,
-  close: false,
-}).refresh();
+// new SimpleLightbox('.gallery-list li', {
+//   showCounter: false,
+//   close: false,
+// }).refresh();
