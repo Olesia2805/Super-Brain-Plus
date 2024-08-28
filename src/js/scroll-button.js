c6d3aaea3384be8bfccc9018f6af474ddb8d0a40
Anna <anjanadvorna@gmail.com>
2024-08-29 00:58:08 +0300

    add advantages and swiper

---
+++ src/js/scroll-button.js
@@ -0,0 +1,24 @@
+// const offset = 100;
+// const scrollUp = document.querySelector('.scroll-button');
+// const scrollUpSvgPath = document.querySelector('.scroll-button-icon');
+// const screenHeight = window.screen.height;
+// // console.log('screenHeight', screenHeight);
+
+// const getTop = () => window.pageYoffset || document.documentElement.scrollTop;
+// console.log(window.pageYoffset);
+
+// window.addEventListener('scroll', () => {
+//   if (getTop() > offset) {
+//     // console.log(getTop());
+//     scrollUp.classList.add('scroll-up--active');
+//   } else {
+//     scrollUp.classList.remove('scroll-up--active');
+//   }
+// });
+
+// scrollUp.addEventListener('click', () => {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth',
+//   });
+// });
--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import './js/header.js';
 import './js/mobile-menu.js';
 import './js/gallery-slider.js';
 import './js/navigation.js';
+import './js/scroll-button.js';
